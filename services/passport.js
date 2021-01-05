ecdf428f4e4d526816fe6ddc2e33b95af1eb0fef
arielyiyitou <my442@drexel.edu>
2021-01-05 17:06:36 -0500

    start react front end, refactor passport part as asyncawait

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,20 @@ passport.use(
         clientID: keys.googleClientID, 
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback',
-        proxy: true                                        // trust any proxy make sure it is https not http
-        }, (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleID: profile.id})
-                .then( (existingUser) => {
-                    if (existingUser) {
-                        // user already exists in database
-                        done(null, existingUser);
-                    } else {
-                        new User({ googleID: profile.id})
-                        .save()
-                        .then(user => done(null, user));
-                    }
-                });
-    })
+        proxy: true                                        // trust any proxy, make sure it is https not http
+        }, 
+
+        // refactored as async await
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleID: profile.id});
+
+            if (existingUser) {
+                return done(null, existingUser);
+            };
+
+            const user = await new User({ googleID: profile.id}).save();
+            done(null, user);
+
+        }
+    )
 );
