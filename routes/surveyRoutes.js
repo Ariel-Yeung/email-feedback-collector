9320ea3d664b3df5efd5add89259e3ee1ab8c690
arielyiyitou <my442@drexel.edu>
2021-07-11 20:20:55 -0400

    Test sendgrid webhook integration

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -23,6 +23,7 @@ module.exports = (app) => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
+    console.log(req.body);
     const p = new Path.createPath('/api/surveys/:surveyId/:choice');
 
     _.chain(req.body)
